20e502342b2d0e17a850c2e4c1de6b075f29f9ce
Wesley Lewis <wdlewis@clemson.edu>
2019-06-05 22:23:08 -0400

    to master

--- 3220/programs/pa3/hybrid.c
+++ 3220/programs/pa3/hybrid.c
@@ -1,9 +1,33 @@
 #include "hybrid.h"
 
 char *allocate (int size) {
-
+  if (size <= 0) {
+    return NULL;
+  }
+  else if (size <= (arena_block_size[0] - 8)) {
+    return bitmap_allocate();
+  }
+  else if (size <= (arena_block_size[1] - 8)) {
+    return list_allocate (1);
+  }
+  else if (size <= (arena_block_size[2] - 8 )) {
+    return list_allocate (2);
+  }
+  else {
+    return NULL;
+  }
 }
 
 void release (char *release_ptr) {
+
+}
+
+char *bitmap_allocate(int size) {
+
+
+}
+  size = 50;
+
+list_allocate (int size) {
 
 }
