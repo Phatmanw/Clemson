52abc26a234a79f63c8dafbca93648b227d6ff80
Wesley Duncan Lewis <wdlewis@titan1.computing.clemson.edu>
2019-09-19 15:55:27 -0400

    networking problems

--- 3600/examples/pa3.py
+++ 3600/examples/pa3.py
@@ -11,25 +11,26 @@ serverPort = 3607
 
 #create sockets and register them with selector
 for i in range(0,3):
-    connectID = i + 1
-    print('starting connection', connectID, 'to', serverName)
-    sock = socket(AF_INET, SOCK_STREAM)
-    sock.setblocking(False)
-    sock.connect_ex((serverName, serverPort))
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(sock, events, i+1)
+	connectID = i + 1
+   print('starting connection', connectID, 'to', serverName)
+   sock = socket(AF_INET, SOCK_STREAM)
+   sock.setblocking(False)
+   sock.connect_ex((serverName, serverPort))
+   events = selectors.EVENT_READ     
+	sel.register(sock, events, i)
+
 
 #
 while True:
-    message = input('input something user: ')
-    sock.send(message.encode())
-    events = sel.select(timeout=10)
+	message = input('input something user: ')
 
-    for key, mask in events:
-        sock = key.fileobj
-        data = key.data
-        if mask & selectors.EVENT_WRITE:
-            response = sock.recv(2048).decode()
-            print(response + ": " + str(data))
-        else:
-            pass
+	for i in range(0,3):
+		events = sel.select(timeout=10)
+		sock.send(message.encode())
+
+
+	for key, mask in events:
+		sock = key.fileobj
+		data = key.data
+		response = sock.recv(2048).decode()
+		print(response + ": " + str(data))
